e7a7c259bb35213b0f95e5fd03581559f6437992
Ryan Wang <hwwangwang@gmail.com>
2020-04-22 09:48:26 +0800

    readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# typing stubs for FlaskSqalchemy
+
+## Install
+
+```bash
+pip install flask-sqlalchemy-stubs
+```
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def find_stub_files():
 
 
 setup(name='flask_sqlalchemy-stubs',
-      version='0.1.1',
+      version='0.2',
       description=description,
       long_description=install_instructions,
       long_description_content_type='text/markdown',
